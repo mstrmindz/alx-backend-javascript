0a986ff51eb7428bc938df9c8af621e0422ee6a5
mstrmindz <tunjexi@yahoo.com>
2023-09-28 15:23:34 +0100

    updates

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Use const to instantiate variables
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let'; // Use let to instantiate variables
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  var task = "I prefer const when I can.";
+  return task;
+}
+
+export function getLast() {
+  return " is okay";
+}
+
+export function taskNext() {
+  var combination = "But sometimes let";
+  combination += getLast();
+
+  return combination;
+}
