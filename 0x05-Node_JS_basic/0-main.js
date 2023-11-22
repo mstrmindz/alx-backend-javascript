77631c087ab7462acdd71d3e86e7c93b195349d5
Mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-11-22 03:07:23 +0000

    Update 0-main.js

--- 0x05-Node_JS_basic/0-main.js
+++ 0x05-Node_JS_basic/0-main.js
@@ -2,5 +2,3 @@
 const displayMessage = require("./0-console");
 
 displayMessage("Hello NodeJS!");
-displayMessage("This is a custom message.");
-// Add more displayMessage calls with different messages if needed
