5035d88109febe9ab7c01b1592d0bb976f5d33c6
Mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-11-24 02:44:48 +0000

    feat: Add individual test cases to 0-calcul.test.js
    Add individual test cases to 0-calcul.test.js and update package.json

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,20 @@
-// 0-calcul.test.js
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', function () {
-  it('should return the sum of rounded numbers', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-});
+// Test case 1
+const result1 = calculateNumber(1, 3);
+assert.strictEqual(result1, 4);
+
+// Test case 2
+const result2 = calculateNumber(1, 3.7);
+assert.strictEqual(result2, 5);
+
+// Test case 3
+const result3 = calculateNumber(1.2, 3.7);
+assert.strictEqual(result3, 5);
+
+// Test case 4
+const result4 = calculateNumber(1.5, 3.7);
+assert.strictEqual(result4, 6);
+
+console.log("All tests passed!");
