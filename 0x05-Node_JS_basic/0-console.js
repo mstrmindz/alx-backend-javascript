7e894c2505602163ebac185c9d29e8506fcdbcd1
mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2024-01-10 21:45:46 -0500

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,12 +1,5 @@
-// 0-console.js
-
-/**
- * Displays the provided message in STDOUT.
- * @param {string} message - The message to be displayed.
- */
 function displayMessage(message) {
-  console.log(message);
+    console.log(message);
 }
 
-// Export the function for external use
 module.exports = displayMessage;
